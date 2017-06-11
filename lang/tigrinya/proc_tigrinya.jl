467b132dbf961c4345afe56100fa83b322e14ae6
Kobic <kobic@voiceitt.com>
2017-06-11 20:10:32 +0300

    freq

--- lang/tigrinya/proc_tigrinya.jl
+++ lang/tigrinya/proc_tigrinya.jl
@@ -7,11 +7,15 @@ consonant_base = j["consonant_base"]
 vowel_base = j["vowel_base"]
 
 for (k,v) in other_words
-    open("out/$k.csv","w") do f
     println("out/$k.csv")
-        println(f,"word,count,trans")
-        for (w,freq) in v
-            println(f,join([w,freq,xlit(w)] ,", "))
+    open("out/$k.csv","w") do f
+        open("out/$k.freq.csv","w") do ff
+            println(f,"word,trans")
+            println(ff,"word,count,trans")
+            for (w,freq) in v
+                println(f,join([w,xlit(w)] ,", "))
+                println(ff,join([w,freq,xlit(w)] ,", "))
+            end
         end
     end
 end
