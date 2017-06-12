17b9822998de443681bb676238f73fcf0bd9730b
Kobic <kobic@voiceitt.com>
2017-06-12 22:23:54 +0300

    rm Kaf dagesh sofit, no such letter, unique list hebrew

---
+++ lang/hebrew/unique_list.jl
@@ -0,0 +1,10 @@
+
+l, titles = readcsv("manual_tmp.csv",header=true);
+u_list = Dict(zip(l[:,1],l[:,2]))
+
+open("manual_unique.csv","w") do f
+  println(f, join(titles,", ") )
+  [println(f,join(kv,",")) for kv in u_list]
+end
+
+true
